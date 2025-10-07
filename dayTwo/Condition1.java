282c7c908110090854ed688fb06b9a3d81886f33
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-07 10:59:57 +0700

    Add initial Java classes for dayOne and dayTwo exercises

---
+++ dayTwo/Condition1.java
@@ -0,0 +1,10 @@
+package dayTwo;
+
+public class Condition1 {
+    public static void main(String[] args) {
+        boolean user = true;
+        if(user == true){
+            System.out.println("User is true");
+        }
+    }
+}
