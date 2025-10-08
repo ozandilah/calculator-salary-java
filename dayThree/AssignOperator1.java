1d979671c8ef979c1c7998cde18d92a332c04717
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-08 10:13:38 +0700

    Add AssignOperator and AssignOperator1 classes to demonstrate assignment and boolean operations

---
+++ dayThree/AssignOperator1.java
@@ -0,0 +1,10 @@
+package dayThree;
+
+public class AssignOperator1 {
+    public static void main(String[] args) {
+        boolean z = true;
+        if(z){
+            System.out.println("Z is true");
+        }
+    }
+}
