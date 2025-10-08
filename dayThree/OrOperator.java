8695d3f939b786ac9fe87c3e0924dec383983bd6
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-08 09:06:17 +0700

    Add OrOperator class to demonstrate logical OR operation in exam scoring

---
+++ dayThree/OrOperator.java
@@ -0,0 +1,11 @@
+package dayThree;
+
+public class OrOperator {
+    public static void main(String[] args) {
+        double examScore = 85;
+        if(examScore >= 60 || examScore <= 100)
+        {
+            System.out.println("The student has passed the exam.");
+        }
+    }
+}
