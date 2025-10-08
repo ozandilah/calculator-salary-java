d28d82c515b0ead3b34a52b9f1425fbba2ee4149
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-08 08:06:37 +0700

    Add logicalOperators and logicalOperators1 classes for dayTwo exercises

---
+++ dayTwo/logicalOperators.java
@@ -0,0 +1,20 @@
+package dayTwo;
+
+public class logicalOperators {
+    public static void main(String[] args) {
+        int age = 19;
+        
+        // if(age > 18){
+        //     // System.err.println("Condition true");
+        // }
+
+        String gender = "male";
+
+        // if(gender == "male"){
+        //     // System.err.println("Gender is male");
+        // }
+        if(age > 18 && gender == "male"){
+            System.err.println("Condition true and Gender is male");
+        }
+    }
+}
---
+++ dayTwo/logicalOperators1.java
@@ -0,0 +1,12 @@
+package dayTwo;
+
+public class logicalOperators1 {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 20, d = 0;
+
+        if(a < b && b == c){
+            d = a + b + c;
+            System.out.println("The value of d is: " + d);
+        }
+    }
+}
