40becf74281d40306abd31dcd94b3f17ee2b3349
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-09 13:46:14 +0700

    Add IfelseCondition, IfelseCondition1, IfelseCondition2, and ladderIfElse classes to demonstrate conditional statements in Java

---
+++ expressionAndStatments/IfelseCondition.java
@@ -0,0 +1,15 @@
+package expressionAndStatments;
+
+public class IfelseCondition {
+    public static void main(String[] args) {
+        int num = -5;
+        if(num > 0)
+        {
+            System.out.println("Number is Positive");
+        }
+        else
+        {
+            System.out.println("Number is Negative");
+        }
+    }
+}
