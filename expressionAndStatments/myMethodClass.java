24779802101b77592fbf58dd24cb5839d2e3933b
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-10 07:42:47 +0700

    Add myMethodClass and paraMethodClass to demonstrate basic method usage in Java

---
+++ expressionAndStatments/myMethodClass.java
@@ -0,0 +1,12 @@
+package expressionAndStatments;
+
+public class myMethodClass {
+    public static void main(String[] args) {
+        printString();
+        
+    }
+
+    public static void printString(){
+        System.out.println("Hello World");
+    }
+}
---
+++ expressionAndStatments/paraMethodClass.java
@@ -0,0 +1,12 @@
+package expressionAndStatments;
+
+public class paraMethodClass {
+    public static void main(String[] args) {
+        Modifiers(1, "John");
+    }
+    public static void Modifiers(int number, String name)
+    {
+        System.out.println("First Parameter Number is value: " + number);
+        System.out.println("Second Parameter Name is value: " + name);
+    }
+}
