2e3a18a7ff9b77a7da7b2f284b90544b69db144c
masozan7 <fauzan.fadilah@epfootwear.com>
2025-10-10 08:28:59 +0700

    Add methodExample1 and methodExercise classes to demonstrate method usage and position calculation in Java

--- expressionAndStatments/methods/methodExample1.java
+++ expressionAndStatments/methods/methodExample1.java
@@ -3,6 +3,8 @@ package expressionAndStatments.methods;
 public class methodExample1 {
     public static void main(String[] args) {
         calculateArea(50, 3.14);
+        double area = calculateArea(10, 3.14);
+        System.out.println("Area of circle is : "+ area);
     }   
     public static void calculateArea(double radius, double pi)
     {
---
+++ expressionAndStatments/methods/methodExample2.java
@@ -0,0 +1,12 @@
+package expressionAndStatments.methods;
+
+public class methodExample2 {
+    public static void main(String[] args) {
+        int sum = Add(5, 10);
+        System.out.println("Sum is: " + sum);
+    }
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
